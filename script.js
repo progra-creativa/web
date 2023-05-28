d6fd37c289bfbac7c18752eef9db66d95f4043e4
gal-pal-gab <134663676+gal-pal-gab@users.noreply.github.com>
2023-05-27 23:11:19 -0400

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Cursor sacado de https://forum.freecodecamp.org/t/making-an-image-follow-the-cursor/358711/2 //
+// Cursor sacado de https://forum.freecodecamp.org/t/making-an-image-follow-the-cursor/358711/2//
 $(document).ready(function(){
   $(document).mousemove(function(e){
     $('img').css('left',e.pageX+"px");
