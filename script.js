c50349b11beca6227b5424b6108a825ba7efce6e
gal-pal-gab <134663676+gal-pal-gab@users.noreply.github.com>
2023-05-27 23:02:01 -0400

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// Cursor sacado de https://forum.freecodecamp.org/t/making-an-image-follow-the-cursor/358711/2 //
+$(document).ready(function(){
+  $(document).mousemove(function(e){
+    $('img').css('left',e.pageX+"px");
+    $('img').css('top',e.pageY+"px");
+  });
+});
+
 // Obtener referencia al elemento de audio
 var audio = document.getElementById("audio-element"); // Reemplaza "audio-element" con el ID o clase de tu elemento de audio
 
